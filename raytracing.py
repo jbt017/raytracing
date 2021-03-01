fc6c4b180ca72d62bfbd521d4da448898fa70aae
jbt017 <jbt017@email.latech.edu>
2021-02-28 18:09:59 -0600

    started foundation

---
+++ raytracing.py
@@ -0,0 +1,9 @@
+# Asignment 5:  Ray Tracing
+# Justin Blake Till CWID 101-66-127
+# Summary: Define three spheres and checkerboard plane.  Light the objects with ray tracing implementation.
+# shading.
+# Date: 2/26/21
+
+import math
+import copy
+from tkinter import *
